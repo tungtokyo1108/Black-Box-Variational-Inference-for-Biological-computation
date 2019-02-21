d47e3813ab7f382c90d7db3a4c05fc2d268a1a11
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-02-21 09:45:08 +0900

    Create IBM_Cars2015_TwitterDataAnalysis.py

---
+++ Python for Data Science/IBM-Data Science Project/IBM_Cars2015_TwitterDataAnalysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 09:47:07 2019
+
+Reference: https://github.com/ibm-watson-data-lab/spark.samples/blob/master/notebook/DashDB%20Twitter%20Car%202015%20Python%20Notebook.ipynb
+
+@author: Tung1108
+"""
+
+import nltk
+nltk.download("stopwords")

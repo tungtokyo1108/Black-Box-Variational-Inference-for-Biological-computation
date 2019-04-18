e3b7cb38342e4967c5aa5fea8081374f30f773bf
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-04-18 22:51:57 +0900

    Update Decision_trees_Harvard.py

--- Technical_Revision/RandomForest/Decision_trees_Harvard.py
+++ Technical_Revision/RandomForest/Decision_trees_Harvard.py
@@ -7,6 +7,7 @@ Reference:
     https://harvard-iacs.github.io/2018-CS109A/lectures/lecture-15/demo/
     https://github.com/Harvard-IACS/2018-CS109A/tree/master/content/lectures/lecture15/presentation       
     https://github.com/PacktPublishing/Artificial-Intelligence-with-Python/blob/master/Chapter%2003/code/decision_trees.py
+    https://github.com/ageron/handson-ml2/blob/master/06_decision_trees.ipynb
     
 @author: tungutokyo
 """
@@ -37,6 +38,12 @@ from statsmodels.tools import add_constant
 from statsmodels.regression.linear_model import RegressionResults
 from sklearn.decomposition import PCA
 from sklearn import ensemble 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from graphviz import Source
+from sklearn.tree import export_graphviz
+from matplotlib.colors import ListedColormap
+from sklearn.datasets import load_iris
 
 """
 fit_and_plot_dt
@@ -47,7 +54,7 @@ Input:
     depth (depth of tree)
 """
 def fit_and_plot_dt(x, y ,depth, title, ax, plot_data=True, fill=True, color='Blues'):
-    dt = tree.DecisionTreeClassifier(max_depth=depth)
+    dt = tree.DecisionTreeClassifier(max_depth=depth, random_state=42)
     dt.fit(x,y)
     
     ax = plot_tree_boundary(x, y, dt, title, ax, plot_data, fill, color)
@@ -96,18 +103,21 @@ def plot_tree_boundary(x, y, model, title, ax, plot_data=True, fill=True, color=
     
     return ax 
 
-npoints = 200
-# Sample two independent N(0.5^2)
-data = np.random.multivariate_normal([0,0], np.eye(2)*5, size=npoints)
-data = np.hstack((data, np.zeros((npoints, 1))))
+data = np.loadtxt('data_decision_trees.txt', delimiter=',')
+X, y = data[:, :-1], data[:, -1]
 
-data[data[:, 0]**2 + data[:,1]**2 < 3**2, 2] = np.random.choice([0,1], len(data[data[:, 0]**2 + data[:,1]**2 < 3**2]), 
-                                                                    p=[0.2, 0.8])
+# npoints = 200
+# Sample two independent N(0.5^2)
+# data = np.random.multivariate_normal([0,0], np.eye(2)*5, size=npoints)
+# data = np.hstack((data, np.zeros((npoints, 1))))
+
+# data[data[:, 0]**2 + data[:,1]**2 < 3**2, 2] = np.random.choice([0,1], len(data[data[:, 0]**2 + data[:,1]**2 < 3**2]), 
+#                                                                    p=[0.2, 0.8])
 fig, ax = plt.subplots(1,1, figsize=(10,8))
-x = data[:, :-1]
-y = data[:, -1]
-ax.scatter(x[y==1, 0], x[y==1, 1], c='green', label='vegetation')
-ax.scatter(x[y==0, 0], x[y==0, 1], c='black', label='non vegetation', alpha=0.25)
+# x = data[:, :-1]
+# y = data[:, -1]
+ax.scatter(X[y==1, 0], X[y==1, 1], c='green', label='vegetation')
+ax.scatter(X[y==0, 0], X[y==0, 1], c='black', label='non vegetation', alpha=0.25)
 ax.set_xlabel('longitude')
 ax.set_ylabel('latitude')
 ax.set_title('satellite image')
@@ -117,61 +127,52 @@ plt.show()
 
 depths = [1, 2, 5, 1000]
 fig, ax = plt.subplots(1, len(depths), figsize=(20,4))
-x = data[:, :-1]
-y = data[:, -1]
+# x = data[:, :-1]
+# y = data[:, -1]
 ind = 0
 for i in depths:
-    ax[ind] = fit_and_plot_dt(x,y,i,'Depth {}'.format(i), ax[ind])
-    ax[ind].set_xlim(-6,6)
-    ax[ind].set_ylim(-6,6)
+    ax[ind] = fit_and_plot_dt(X,y,i,'Depth {}'.format(i), ax[ind])
+    ax[ind].set_xlim(0,10)
+    ax[ind].set_ylim(0,10)
     ind += 1
 
+# Variance comparison between simple and complex models 
+depths = [3,5,1000]
+fig, ax = plt.subplots(1, len(depths), figsize=(15,5))
+for d in range(len(depths)):
+    for i in range(10):
+        ax[d] = fit_and_plot_dt(X,y,depths[d], 'Depth {}'.format(depths[d]), ax[d], plot_data=False, fill=False)
+        # ax[d].set_xlim(0,10)
+        # ax[d].set_ylim(0,10)
+plt.tight_layout()
+plt.show()
 
+# Difference Splitting Criteria 
+depth = 10
+fig, ax = plt.subplots(1,2,figsize=(10,5))
+dt = tree.DecisionTreeClassifier(max_depth=depth)
+dt.fit(X,y)
+ax[0] = plot_tree_boundary(X,y,dt,'Gini',ax[0],color='Reds')
 
+dt = tree.DecisionTreeClassifier(max_depth=depth, criterion='entropy')
+dt.fit(X,y)
+ax[1] = plot_tree_boundary(X,y,dt,'Entropy',ax[1],color='Reds')
+plt.tight_layout()
+plt.show()
 
+# Difference Stopping Conditions 
+fig, ax = plt.subplots(1,3, figsize=(15,5))
+dt = tree.DecisionTreeClassifier()
+dt.fit(X,y)
+ax[0] = plot_tree_boundary(X,y,dt,'No Stopping Condition',ax[0])
 
+dt = tree.DecisionTreeClassifier(min_impurity_split=0.32)
+dt.fit(X,y)
+ax[1] = plot_tree_boundary(X,y,dt,'Minimum Purity Split = 0.32',ax[1])
 
+dt = tree.DecisionTreeClassifier(min_samples_leaf=2)
+dt.fit(X,y)
+ax[2] = plot_tree_boundary(X,y,dt,'Minimum Samples per leaf = 10', ax[2])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.tight_layout()
+plt.show()

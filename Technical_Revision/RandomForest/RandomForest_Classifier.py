db01b644b0a3d00e566f084de88f39137a6ab260
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-04-28 23:20:58 +0900

    Update RandomForest_Classifier.py

--- Technical_Revision/RandomForest/RandomForest_Classifier.py
+++ Technical_Revision/RandomForest/RandomForest_Classifier.py
@@ -102,3 +102,51 @@ print("Accuracy, Training Set: {:.2%}".format(accuracy_score(ytrain, model.predi
 print("Accuracy, Testing Set: {:.2%}".format(accuracy_score(ytest, model.predict(Xtest))))
 
 pd.crosstab(ytest, model.predict(Xtest), margins=True, rownames=['Actual'], colnames=['Predicted'])
+
+###############################################################################
+###################### Random Forest and Bagging ##############################
+###############################################################################
+
+model = RandomForestClassifier(n_estimators=int(Xtrain.shape[1]/2), max_depth=best_depth)
+model.fit(Xtrain, ytrain)
+
+y_pred_train = model.predict(Xtrain)
+y_pred_test = model.predict(Xtest)
+
+train_score = accuracy_score(ytrain, y_pred_train)*100
+test_score = accuracy_score(ytest, y_pred_test)*100
+
+print("Accuracy, Training Set :", str(train_score)+'%')
+print("Accuracy, Testing Set :", str(test_score)+'%')
+
+feature_importance = model.feature_importances_
+feature_importance = 100*(feature_importance / feature_importance.max())
+sorted_idx = np.argsort(feature_importance)
+pos = np.arange(sorted_idx.shape[0]) + .5
+
+plt.figure(figsize=(12,12))
+plt.barh(pos, feature_importance[sorted_idx], align='center')
+plt.yticks(pos, Xtrain.columns[sorted_idx], fontsize=10)
+plt.xlabel('Relative Importance', fontsize=15)
+plt.title('Variable Importance', fontsize=20)
+
+###############################################################################
+###################### Compare with other models ##############################
+###############################################################################
+
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+log_clf = LogisticRegression(solver="lbfgs", random_state=42)
+rnd_clf = RandomForestClassifier(n_estimators=int(Xtrain.shape[1]/2), max_depth=best_depth, random_state=42)
+svm_clf = SVC(gamma="scale", random_state=42)
+
+voting_clf = VotingClassifier(estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)], voting='hard')
+voting_clf.fit(Xtrain, ytrain)
+
+for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+    clf.fit(Xtrain, ytrain)
+    y_pred = clf.predict(Xtest)
+    print(clf.__class__.__name__, accuracy_score(ytest, y_pred))
+    

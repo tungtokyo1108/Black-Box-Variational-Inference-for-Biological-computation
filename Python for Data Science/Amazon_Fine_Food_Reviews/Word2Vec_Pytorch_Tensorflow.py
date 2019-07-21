cbd0e7ac57fd6aa0662ca6bf1de793f27b1a16f4
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-07-21 23:27:28 +0900

    Update Word2Vec_Pytorch_Tensorflow.py

--- Python for Data Science/Amazon_Fine_Food_Reviews/Word2Vec_Pytorch_Tensorflow.py
+++ Python for Data Science/Amazon_Fine_Food_Reviews/Word2Vec_Pytorch_Tensorflow.py
@@ -35,7 +35,7 @@ sampled_dataset = pd.read_sql_query(""" SELECT * FROM Reviews """, connection_sq
 
 X = sampled_dataset["CleanedText"].values
 
-split = 50000
+split = 500
 X_train = X[0:split,]
 X_test = X[split:100000]
 y_test = y[split:100000]
@@ -266,56 +266,78 @@ tsne_plot(tsne_30, y_train, 30)
 
 
 ###############################################################################
-################ Word2Vec implemented by Pytorch ###########################
+################### Word2Vec implemented by Pytorch ###########################
 ###############################################################################
 
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import matplotlib.pyplot as plt 
 
+dtype = torch.FloatTensor
 
+word_sequence = " ".join(X_train).split()
 
+word_list = " ".join(X_train).split()
+word_list = list(set(word_list))
+word_dict = {w: i for i, w in enumerate(word_list)}
 
+batch_size = 20
+embedding_size = 2
+voc_size = len(word_list)
 
+def random_batch(data, size):
+    random_inputs = []
+    random_labels = []
+    random_index = np.random.choice(range(len(data)), size, replace=False)
+    for i in random_index:
+        random_inputs.append(np.eye(voc_size)[data[i][0]])
+        random_labels.append(data[i][1])
+        
+    return random_inputs, random_labels
 
+skip_grams = []
+for i in range(1, len(word_sequence) - 1):
+    target = word_dict[word_sequence[i]]
+    context = [word_dict[word_sequence[i - 1]], word_dict[word_sequence[i + 1]]]
+    for w in context:
+        skip_grams.append([target, w])
 
+class Word2Vec(nn.Module):
+    def __init__(self):
+        super(Word2Vec, self).__init__()
+        self.W = nn.Parameter(-2 * torch.rand(voc_size, embedding_size) + 1).type(dtype)
+        self.WT = nn.Parameter(-2 * torch.rand(embedding_size, voc_size) + 1).type(dtype)
+        
+    def forward(self, X):
+        hidden_layer = torch.matmul(X, self.W)
+        output_layer = torch.matmul(hidden_layer, self.WT)
+        return output_layer
 
+model = Word2Vec()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+for epoch in range(5000):
+    input_batch, target_batch = random_batch(skip_grams, batch_size)
+    input_batch = Variable(torch.Tensor(input_batch))
+    target_batch = Variable(torch.LongTensor(target_batch))
+    
+    optimizer.zero_grad()
+    output = model(input_batch)
+    
+    loss = criterion(output, target_batch)
+    if (epoch + 1)%1000 == 0:
+        print("Epoch: ", "%04d" % (epoch + 1), "cost =", "{:.6f}".format(loss))
+    
+    loss.backward()
+    optimizer.step()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plt.figure(figsize=(15,15))    
+for i, label in enumerate(word_list):
+    W, WT = model.parameters()
+    x,y = float(W[i][0]), float(W[i][1])
+    plt.scatter(x,y)
+plt.show()

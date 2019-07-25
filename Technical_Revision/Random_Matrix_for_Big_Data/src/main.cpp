a6c00540124653e90b8ffc4956e613219484993a
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-07-25 19:55:04 +0900

    Update main.cpp

--- Technical_Revision/Random_Matrix_for_Big_Data/src/main.cpp
+++ Technical_Revision/Random_Matrix_for_Big_Data/src/main.cpp
@@ -109,32 +109,13 @@ void RunMatrixTest()
 }
 /////////////////////////////////////////////////////////////////////////////////////
 
-void TestsampleGauss()
-{
-    Eigen::MatrixXd m1(10,10);
-
-    RandSVD::Internal::Util<Eigen::MatrixXd> randmat;
-    randmat.sampleGaussianMat(m1);
-    std::cout << "Here is the Gaussian matrix with double type: \n" << m1 << std::endl;
-    // randmat.processGramSchmidt(m1);
-    randmat.modifiedGramSchmit(m1);
-    std::cout << "Here is the GramSchmidt process with double type: \n" << m1 << "\n" << std::endl;
-
-    Eigen::MatrixXf m2(10,10);
-    RandSVD::Internal::Util<Eigen::MatrixXf> randmat_f;
-    randmat_f.sampleGaussianMat(m2);
-    std::cout << "Here is the Gaussian matrix with float type: \n" << m2 << std::endl;
-    randmat_f.modifiedGramSchmit(m2);
-    std::cout << "Here is the GramSchmidt process with float type: \n" << m2 << std::endl;
-}
-
 int main() {
     try
     {
         // Test_Matrix_free_solver();
         // Test_L2norm();
         // Test_SVD();
-        TestsampleGauss();
+        RandSVD::Internal::TestsampleGauss();
         // RunMatrixTest<int>();
     }
     catch(const std::exception& e)

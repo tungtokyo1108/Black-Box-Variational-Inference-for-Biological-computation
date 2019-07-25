4436f37ad6283c61174e5e31a30671cfea51b61c
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-07-25 19:54:34 +0900

    Update utils.cpp

--- Technical_Revision/Random_Matrix_for_Big_Data/src/utils.cpp
+++ Technical_Revision/Random_Matrix_for_Big_Data/src/utils.cpp
@@ -14,55 +14,23 @@
 namespace RandSVD {
     namespace Internal {
         
-        /* *const double SVD_EPS = 0.0001f;
-        
-        void Util::sampleTwoGaussian(double& f1, double& f2) { 
-            double v1 = (double)(std::rand() + 1.f) / ((double)RAND_MAX + 2.f);
-            double v2 = (double)(std::rand() + 1.f) / ((double)RAND_MAX + 2.f);
-            double len = std::sqrt(-2.f * std::log(v1));
-            f1 = len * std::cos(2.f * M_PI * v2);
-            f2 = len * std::sin(2.f * M_PI * v2);
-        }
+        void TestsampleGauss()
+        {
+            Eigen::MatrixXd m1(10,10);
 
-        void Util::sampleGaussianMat(Eigen::MatrixXd& mat) {
-            for (int i=0; i < mat.rows(); ++i)
-            {
-                int j=0;
-                for (; j+1 < mat.cols(); j += 2) 
-                {
-                    double f1, f2;
-                    sampleTwoGaussian(f1,f2);
-                    mat(i,j) = f1;
-                    mat(i,j+1) = f2;
-                }
-                for (; j < mat.cols(); j++)
-                {
-                    double f1, f2;
-                    sampleTwoGaussian(f1,f2);
-                    mat(i,j) = f1;
-                }
-            }
-        }
+            RandSVD::Internal::Util<Eigen::MatrixXd> randmat;
+            randmat.sampleGaussianMat(m1);
+            std::cout << "Here is the Gaussian matrix with double type: \n" << m1 << std::endl;
+            // randmat.processGramSchmidt(m1);
+            randmat.modifiedGramSchmit(m1);
+            std::cout << "Here is the GramSchmidt process with double type: \n" << m1 << "\n" << std::endl;
 
-        void Util::processGramSchmidt(Eigen::MatrixXd& mat) {
-            for (int i=0; i < mat.cols(); ++i)
-            {
-                for (int j=0; j < i; ++j)
-                {
-                    double r = mat.col(i).dot(mat.col(j));
-                    mat.col(i) -= r * mat.col(j);
-                }
-                double norm = mat.col(i).norm();
-                if (norm < SVD_EPS)
-                {
-                    for (int k=i; k < mat.cols(); ++k)
-                    {
-                        mat.col(k).setZero();
-                    }
-                    return;
-                }
-                mat.col(i) *= (1.f / norm);
-            }
-        }*/
+            Eigen::MatrixXf m2(10,10);
+            RandSVD::Internal::Util<Eigen::MatrixXf> randmat_f;
+            randmat_f.sampleGaussianMat(m2);
+            std::cout << "Here is the Gaussian matrix with float type: \n" << m2 << std::endl;
+            randmat_f.modifiedGramSchmit(m2);
+            std::cout << "Here is the GramSchmidt process with float type: \n" << m2 << std::endl;
+        }
     }
 }

06cc5701a967eaa98901e3e572fb607cbb3cff76
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-07-27 22:44:41 +0900

    Create Compound.java

---
+++ Analysis-of-Biological-Data/Big_Data_Analysis/core/sequence/template/Compound.java
@@ -0,0 +1,13 @@
+package jp.ac.utokyo.hashingproject.core.sequence.template;
+
+public interface Compound {
+    public boolean equalsIgnoreCase(Compound compound);
+    public String getDescription();
+    public void setDescription(String description);
+    public String getShortName();
+    public void setShortName(String shortName);
+    public String getLongName();
+    public void setLongName(String longName);
+    public Float getMolecularWeight();
+    public void setMolecularWeight(Float molecularWeight);
+}
